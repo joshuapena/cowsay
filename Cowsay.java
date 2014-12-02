d3ba7b22938ed5e97f34a61aad832b54eecc8a01
Joshua Pena <penacalata@gmail.com>
2014-12-02 12:02:32 -0800

    its beginning to look a lot line christmas

--- Cowsay.java
+++ Cowsay.java
@@ -68,7 +68,7 @@ class Cowsay {
 
         int currentLen = 0;
         int totalLen = 0;
-        String[] lines = new String[length / 40 + 1];
+        String[] lines = new String[length / MAXLENGTH + 1];
         String line = "";
         String top = " ";
         String bottom = " ";
@@ -93,6 +93,7 @@ class Cowsay {
                 line = words[i] + " ";
                 currentLen = 0;
             } else {
+                // otherwise just adds word to line string
                 line += words[i] + " ";
             }
         }
@@ -100,33 +101,53 @@ class Cowsay {
         lines[lines.length - 1] = line;
 
         // What is the length of the longest line
-        int longestLength = lines[0].length();
-        for (int i = 0; i < lines.length; i++) {
-            if (lines[i].length() > longestLength) {
-                longestLength = lines[i].length();
-            }
-        }
+        int lineSize = longestLine(lines);
 
-        lines[lines.length - 1] = finalLine(longestLength, line);
+        // sets the final line
+        lines[lines.length - 1] = finalLine(lineSize, line);
 
-        for (int i = 0; i < longestLength + 1; i++) {
+        // gets the top and bottom to the correct size
+        for (int i = 0; i < lineSize + 1; i++) {
             top += "_";
             bottom += "-";
         }
 
+        // prints out the text
         System.out.print(top + "\n");
-        System.out.print("/ " + finalLine(longestLength, lines[0]) + "\\\n");
+        System.out.print("/ " + finalLine(lineSize, lines[0]) + "\\\n");
         for (int i = 1; i < lines.length - 1; i++) {
-            System.out.print("| " + finalLine(longestLength, lines[i]) + "|\n");
+            if (lines[i] == null) {
+                continue;
+            }
+            System.out.print("| " + 
+                    finalLine(lineSize, lines[i]) + "|\n");
         }
         System.out.print("\\ " + lines[lines.length - 1] + "/\n ");
         System.out.print(bottom + "\n");
         printCow();
     }
+
+    // Gets the longest length of the array
+    public static int longestLine(String[] lines) {
+        int lineSize = lines[0].length();
+        try {
+            for (int i = 0; i < lines.length; i++) {
+                if (lines[i].length() > lineSize) {
+                    lineSize = lines[i].length();
+                }
+            }
+        } catch (NullPointerException e) {
+        }
+        return lineSize;
+    }
+    // puts the spaces needed to reach the wanted length
     public static String finalLine(int length, String line) {
         String space = "";
-        for (int i = 0; i < length - line.length(); i++) {
-            space += " ";
+        try {
+            for (int i = 0; i < length - line.length(); i++) {
+                space += " ";
+            }
+        } catch (NullPointerException e) {
         }
         return line + space;
     }
