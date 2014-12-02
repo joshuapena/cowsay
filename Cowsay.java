0d70b58b67bd7fb09382c3311c4aaf4a078addf0
Joshua Pena <penacalata@gmail.com>
2014-12-02 10:04:18 -0800

    prints out cow

--- Cowsay.java
+++ Cowsay.java
@@ -1,10 +1,35 @@
 import java.util.*;
 
 class Cowsay {
+    static void printCow() {
+        System.out.print("        \\   ^__^\n" +
+                "         \\  (oo)\\_______\n" +
+                "            (__)\\       )\\/\\\n" +
+                "                 ||----w |\n" +
+                "                 ||     ||\n");
+    }
+
     public static void main(String args[]) {
+        int stringLen = 0;
+        String words = "< ";
+        String top = " ";
+        String bottom = " ";
+
         for (int i = 0; i < args.length; i++) {
-            System.out.print(args[i] + " ");
+            stringLen += args[i].length();
+            words += args[i] + " ";
         }
+        words += ">";
+
+        for (int i = 0; i < stringLen + 4; i++) {
+            top += "_";
+            bottom += "-";
+        }
+
+        System.out.print(top + "\n");
+        System.out.print(words + "\n");
+        System.out.print(bottom + "\n");
+        printCow();
         System.out.println();
     }
 }
