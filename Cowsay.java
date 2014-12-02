ba31d47644a7d4a6e3ade8cac0abecc2a2d04fbf
Joshua Pena <penacalata@gmail.com>
2014-12-02 11:44:36 -0800

    multiple lines work

--- Cowsay.java
+++ Cowsay.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class Cowsay {
+    static enum Animal { cow };
     // Prints out the cow
     static void printCow() {
         System.out.print("        \\   ^__^\n" +
@@ -14,6 +15,7 @@ class Cowsay {
 
     public static void main(String args[]) {
         int totalLength = 0;
+        Animal animal = null;
 
         // Calculates the length of the string inputed
         for (int i = 0; i < args.length; i++) {
@@ -23,13 +25,13 @@ class Cowsay {
         // Checks if needed string is one or multiple lines
         // and calls respective funtion
         if (totalLength > MAXLENGTH) {
-            printMultipleLines(args);
+            printMultipleLines(args, totalLength);
         } else {
-            printSingleLine(args);
+            printSingleLine(args, animal);
         }
     }
 
-    public static void printSingleLine(String[] words) {
+    public static void printSingleLine(String[] words, Animal animal) {
         int totalLen = 0;
         String line = "< ";
         String top = " ";
@@ -49,41 +51,83 @@ class Cowsay {
 
         // Prints out the cow and words
         System.out.print(top + "\n" + line + "\n" + bottom + "\n");
-        printCow();
+        switch (animal) {
+            case cow:
+                printCow();
+                break;
+            default:
+                printCow();
+                break;
+        }
         System.out.println();
     }
-    public static void printMultipleLines(String[] words) {
-        System.out.print("foo\n");
-
-        // Check for strings of 40
+    public static void printMultipleLines(String[] words, int length) {
         // put each new line into array
         // work out the sides for the string
         // work out the top
-        /*
+
         int currentLen = 0;
         int totalLen = 0;
-        String words = "< ";
+        String[] lines = new String[length / 40 + 1];
+        String line = "";
         String top = " ";
         String bottom = " ";
+        boolean empty = false;
 
-        for (int i = 0; i < args.length; i++) {
-            totalLen += args[i].length();
-            currentLen += args[i].length() + 1;
+        // Goes through the lines and puts them in the words array
+        for (int i = 0; i < words.length; i++) {
+            totalLen += words[i].length();
+            currentLen += words[i].length() + 1;
+
+            // Check for strings of 40
             if (currentLen > MAXLENGTH) {
-                words += "\n" + args[i];
+                // puts the line in the lines array by searching
+                // for the empty space
+                for (int j = 0; j < lines.length; j++) {
+                    if (lines[j] == null) {
+                        lines[j] = line;
+                        break;
+                    }
+                }
+
+                line = words[i] + " ";
                 currentLen = 0;
             } else {
-                words += args[i] + " ";
+                line += words[i] + " ";
             }
-            System.out.print(currentLen + "\n");
         }
-        words += ">";
+        // Puts the line in the last index
+        lines[lines.length - 1] = line;
 
-        for (int i = 0; i < totalLen + 4; i++) {
+        // What is the length of the longest line
+        int longestLength = lines[0].length();
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].length() > longestLength) {
+                longestLength = lines[i].length();
+            }
+        }
+
+        lines[lines.length - 1] = finalLine(longestLength, line);
+
+        for (int i = 0; i < longestLength + 1; i++) {
             top += "_";
             bottom += "-";
         }
-        */
 
+        System.out.print(top + "\n");
+        System.out.print("/ " + finalLine(longestLength, lines[0]) + "\\\n");
+        for (int i = 1; i < lines.length - 1; i++) {
+            System.out.print("| " + finalLine(longestLength, lines[i]) + "|\n");
+        }
+        System.out.print("\\ " + lines[lines.length - 1] + "/\n ");
+        System.out.print(bottom + "\n");
+        printCow();
+    }
+    public static String finalLine(int length, String line) {
+        String space = "";
+        for (int i = 0; i < length - line.length(); i++) {
+            space += " ";
+        }
+        return line + space;
     }
 }
